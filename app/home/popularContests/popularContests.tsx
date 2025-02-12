56199e97c8bd8157c35251c55ea21b0faf180d89
khkiiim01 <khkiiim01@gmail.com>
2025-02-13 01:14:47 +0900

    For saving(Do not Merge)

---
+++ app/home/popularContests/popularContests.tsx
@@ -0,0 +1,16 @@
+import styles from "./popularContests.module.css"
+
+const PopularContests = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>
+
+            </div>
+            <div className={styles.contests}>
+                
+            </div>
+        </div>
+    );
+};
+
+export default PopularContests;
